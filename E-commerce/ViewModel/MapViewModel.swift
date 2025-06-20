dbfb0f42bf4abfcca2a727023bddc10059341293
kemad97 <90790284+kemad97@users.noreply.github.com>
2025-06-20 18:46:16 +0300

    updated mapviewmodel

--- E-commerce/ViewModel/MapViewModel.swift
+++ E-commerce/ViewModel/MapViewModel.swift
@@ -10,10 +10,10 @@ import MapKit
 import SwiftUI
 
 
-class MapViewModel: ObservableObject {
+class MapViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
     @Published var region = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.4194), // Default: San Francisco
-        span: MKCoordinateSpan(latitudeDelta: 0.09, longitudeDelta: 0.09)
+        center: CLLocationCoordinate2D(latitude: 30.0444, longitude: 31.2357), // Cairo, Egypt
+        span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5) // City-wide view
     )
     @Published var selectedLocation: IdentifiableLocation?
     @Published var selectedAddress: String?
@@ -25,38 +25,65 @@ class MapViewModel: ObservableObject {
         let coordinate: CLLocationCoordinate2D
     }
 
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+
     func requestLocationPermission() {
-//        locationManager.requestWhenInUseAuthorization()
-//        if CLLocationManager.locationServicesEnabled() {
-//            if locationManager.authorizationStatus == .authorizedWhenInUse || locationManager.authorizationStatus == .authorizedAlways {
-//                if let userLocation = locationManager.location?.coordinate {
-//                    region = MKCoordinateRegion(
-//                        center: userLocation,
-//                        span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
-//                    )
-//                }
-//            }
-//        }
         locationManager.requestWhenInUseAuthorization()
     }
 
-    func handleMapTap(at point: CGPoint, in region: MKCoordinateRegion, mapSize: CGSize) {
-        // Convert tap point to map coordinates
-        let normalizedX = point.x / mapSize.width // 0 to 1
-        let normalizedY = point.y / mapSize.height // 0 to 1
+    func centerOnUserLocation() {
+        guard locationManager.authorizationStatus == .authorizedWhenInUse ||
+              locationManager.authorizationStatus == .authorizedAlways else {
+            print("Location permission not granted")
+            return
+        }
+        locationManager.startUpdatingLocation()
+    }
 
-        // Calculate longitude and latitude based on region
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        DispatchQueue.main.async {
+            self.region = MKCoordinateRegion(
+                center: location.coordinate,
+                span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+            )
+            print("Map centered on: \(location.coordinate.latitude), \(location.coordinate.longitude)")
+        }
+        locationManager.stopUpdatingLocation()
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Location error: \(error.localizedDescription)")
+    }
+
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus {
+        case .authorizedWhenInUse, .authorizedAlways:
+            centerOnUserLocation()
+        case .denied, .restricted:
+            print("Location access denied or restricted")
+        case .notDetermined:
+            print("Location permission not yet determined")
+        @unknown default:
+            print("Unknown authorization status")
+        }
+    }
+
+    func handleMapTap(at point: CGPoint, in region: MKCoordinateRegion, mapSize: CGSize) {
+        let normalizedX = point.x / mapSize.width
+        let normalizedY = point.y / mapSize.height
         let regionRadiusLat = region.span.latitudeDelta / 2
         let regionRadiusLon = region.span.longitudeDelta / 2
-
         let minLat = region.center.latitude - regionRadiusLat
         let maxLat = region.center.latitude + regionRadiusLat
         let minLon = region.center.longitude - regionRadiusLon
         let maxLon = region.center.longitude + regionRadiusLon
-
         let latitude = maxLat - (normalizedY * (maxLat - minLat))
         let longitude = minLon + (normalizedX * (maxLon - minLon))
-
         let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
         selectedLocation = IdentifiableLocation(coordinate: coordinate)
         reverseGeocode(coordinate: coordinate)
@@ -85,17 +112,6 @@ class MapViewModel: ObservableObject {
             }
         }
     }
-    
-    func centerOnUserLocation() {
-           guard let userLocation = locationManager.location?.coordinate else {
-               print("Location unavailable")
-               return
-           }
-           region = MKCoordinateRegion(
-               center: userLocation,
-               span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05) // Smaller span for precision
-           )
-       }
 
     func resetSelectedLocation() {
         selectedLocation = nil
